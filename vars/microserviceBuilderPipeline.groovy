c2a2d72099f3ff46efb5622e19d023b9ec093d3b
Michael Kalantar <kalantar@us.ibm.com>
2018-04-29 23:31:40 +0000

    use latest image

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -15,7 +15,7 @@
     dockerImage = 'ibmcom/docker:17.10'
     kubectlImage = 'ibmcom/k8s-kubectl:v1.8.3'
     helmImage = 'lachlanevenson/k8s-helm:v2.7.2'
-    rhelmImage = 'kalantar/rhelm:v2.7.2b'
+    rhelmImage = 'kalantar/rhelm:v2.7.2c'
 
   You can also specify:
 
@@ -53,7 +53,7 @@ def call(body) {
   def docker = (config.dockerImage == null) ? 'ibmcom/docker:17.10' : config.dockerImage
   def kubectl = (config.kubectlImage == null) ? 'ibmcom/k8s-kubectl:v1.8.3' : config.kubectlImage
   def helm = (config.helmImage == null) ? 'lachlanevenson/k8s-helm:v2.7.2' : config.helmImage
-  def rhelm = (config.rhelmImage == null) ? 'kalantar/rhelm:v2.7.2b' : config.rhelmImage
+  def rhelm = (config.rhelmImage == null) ? 'kalantar/rhelm:v2.7.2c' : config.rhelmImage
   def mvnCommands = (config.mvnCommands == null) ? 'clean package' : config.mvnCommands
   def registry = (env.REGISTRY ?: "").trim()
   if (registry && !registry.endsWith('/')) registry = "${registry}/"
